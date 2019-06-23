7c3e2be5275739fcacd893d62889d254fdf0eda9
Adam Morton <adammorton9@gmail.com>
2019-06-23 08:36:41 -0700

    Adding functionality to Export Service. Renaming Console project to ConsoleUI.

--- PasswordChanger.Application/Services/ExportService.cs
+++ PasswordChanger.Application/Services/ExportService.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordChanger.Application.Services
 {
     public class ExportService
     {
-        public void WriteDataTableToCsv(DataTable dataTable)
+        /// <summary>
+        /// Creates and exports a CSV file from a DataTable.
+        /// </summary>
+        public void WriteDataTableToCsv(DataTable dataTable, string saveFilePath, bool includeHeaders)
         {
+            if (!saveFilePath.EndsWith(".csv"))
+            {
+                return;
+            }
 
+            using (StreamWriter writer = new StreamWriter(saveFilePath))
+            {
+                if (includeHeaders)
+                {
+                    IEnumerable<string> headerValues = dataTable.Columns
+                        .OfType<DataColumn>()
+                        .Select(column => QuoteValue(column.ColumnName));
+
+                    writer.WriteLine(string.Join(",", headerValues));
+                }
+
+                IEnumerable<string> items = null;
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    items = row.ItemArray.Select(o => QuoteValue(o?.ToString() ?? string.Empty));
+                    writer.WriteLine(String.Join(",", items));
+                }
+
+                writer.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Adding quotes to handle special characters.
+        /// </summary>
+        private static string QuoteValue(string value)
+        {
+            return string.Concat("\"",
+            value.Replace("\"", "\"\""), "\"");
         }
     }
 }
