10168f432a2a678709ceab4a373a233397b9435b
Adam Morton <adammorton9@gmail.com>
2019-06-23 09:11:25 -0700

    Adding domain layer.

---
+++ PasswordChanger.Domain/OpenOrderUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChanger.Domain
+{
+    public class OpenOrderUser
+    {
+        public int Id { get; }
+        public string Username { get; }
+        public string Password { get; }
+        public Company Company { get; }
+
+        public OpenOrderUser(int id, string username, string password, Company company)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Company = company;
+        }
+    }
+}
